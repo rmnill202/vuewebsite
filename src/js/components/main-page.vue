e2ba3ff11233117786fd832cf654e350739c0728
Robert Nill <rmn6979@rit.edu>
2018-01-28 10:31:44 -0500

    Main page component created
    Added the main page component, setup some temporary headers and removed the boilerplate Hello.vue file.

---
+++ src/js/components/main-page.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2>Header</h2>
+    <h1>Welcome</h1>
+    <h1>Projects</h1>
+    <h1>About</h1>
+    <h1>Skillsets</h1>
+    <h1>Contact</h1>
+  </div>
+</template>
+
+
+<script>
+  import WebsiteData from '../../assets/website-data.json'
+
+  export default {
+    name: "main-page",
+    data() {
+      return {
+
+      };
+    },
+    mounted() {
+      console.log("Hello world!");
+      console.log(WebsiteData);
+    }
+  }
+
+</script>
+
+
+<style>
+
+</style>
