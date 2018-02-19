b90ec4370de7874a3399b150320061242304795c
Robert Nill <rmn6979@rit.edu>
2018-02-19 15:00:38 -0500

    Added external links
    Added some external links for the left icons, should just about wrap up the initial toolbar implementation.

--- src/js/components/header-nav.vue
+++ src/js/components/header-nav.vue
@@ -1,23 +1,22 @@
 <template>
   <div>
     <v-toolbar flat dense>
-      <v-btn icon v-for="link in leftLinks">
+      <!-- Icons on the left side of the toolbar -->
+      <v-btn icon v-for="link in leftLinks" :href="link.address">
         <v-icon>{{link.icon.icon}}</v-icon>
       </v-btn>
 
       <v-spacer></v-spacer>
 
-      <!-- For larger screens -->
+      <!-- For larger screens, display all buttons -->
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn v-for="link in rightLinks" flat>{{link.text}}</v-btn>
       </v-toolbar-items>
 
-      <!-- For smaller screens -->
+      <!-- For smaller screens, display a menu -->
       <v-menu transition="slide-x-transition" class="hidden-md-and-up">
-
         <!-- Hamburger menu icon! -->
         <v-btn icon slot="activator"> <v-icon>menu</v-icon> </v-btn>
-
         <!-- The menu contents -->
         <v-list>
           <v-list-tile @click="" v-for="link in rightLinks">
@@ -25,7 +24,6 @@
           </v-list-tile>
         </v-list>
       </v-menu>
-
     </v-toolbar>
   </div>
 </template>
