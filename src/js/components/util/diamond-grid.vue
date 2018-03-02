ffca864c63401ad7dca2ab2710f28a7d5e93112e
Robert Nill <rmn6979@rit.edu>
2018-03-01 22:27:22 -0500

    More logic outlining
    Outlining more of the logic behind rendering each row. I think the distinction between top/other rows (which impacts the use of a negative top-margin) might be unnecessary, if I just add a v-if statement.
    
    I'll probably rework the algorithm for generating rows at some point, but let's not pre-optimize.

--- src/js/components/util/diamond-grid.vue
+++ src/js/components/util/diamond-grid.vue
@@ -1,37 +1,135 @@
 <!-- Header goes here -->
 <template>
   <div>
-    <!--
-    //Render the top row
-    //Render the remaining rows using a v-for loop
-    -->
+    <!-- Start off by rendering the top row -->
+    <div v-if="topRow">
+      <img v-for="image in topRow" :key="image.id" :src="image">
+    </div>
+
+    <!-- Now render the rest of the content, if it exists -->
+    <div v-for="row in bottomRows" :key="row.id">
+      <img v-for="image in row" :key="image.id" :src="image" :class="imgClass">
+    </div>
   </div>
 </template>
 
 <script>
   export default {
     name: 'diamond-grid',
-    props: ['pattern', 'offsets', 'images', 'spacing', 'size', 'startFrom'],
-    data() {
-      return {
-
+    props: {
+      /**
+       * Defines the number of images/visual elements used for [n] rows, where [n] is the size of the array passed
+       *   in as the pattern. The pattern will be repeated as many times as necessary for all images passed in to be
+       *   displayed.
+       *
+       *  By default, patterns begin to fill the grid from the bottom left.
+       *
+       *  A pattern is contained within an object/array, and contains one of two types of pattern elements:
+       *    1. Integers that define the number of images each row has.
+       *    2. Arrays containing the characters 'i' or 'x', which represent 'image' and 'empty space' respectively.
+       *
+       *  An example containing both, and creates a grid of diamonds with an empty center.
+       *  [ 1, 2, [i, x, i], 2, 1 ]
+       */
+      pattern: {
+        type: Object,
+        required: true
+      },
+      /**
+       * An array of image URLs to populate the grid with.
+       */
+      images: {
+        type: Object,
+        required: true
+      },
+      /**
+       * TODO: 
+       */
+      offsets: {},
+      /**
+       * TODO:
+       */
+      spacing: {},
+      /**
+       * Defines what vertical direction new rows are added from.
+       */
+      rowStart: {
+        type: String,
+        required: false,
+        default: 'bottom'
+      },
+      /**
+       * What side each row should start populating with images. Either 'left' or 'right'.
+       */
+      columnStart: {
+        type: String,
+        required: false,
+        default: 'left'
       }
     },
+    data() {
+      return {}
+    },
     computed: {
+      /** 
+       * Based on the given list of images and the pattern, construct an array of image rows,
+       *  where the first row is what will be rendered at the top of the page. 
+       */
       rows() {
-        //First, let's take the length of images and fit the pattern to that length
-          //ex: 5, pattern is 1-2. We get 1-2-1-2 (1, 23, 4, 5x) (x is added, but is invisible)
-        //Now, let's reverse the pattern order, and therefore the indices
+        if(!this.pattern || (this.images instanceof Array))
+          return false;
+        
+        //Create an array of rows in order of the pattern. It'll be reversed later
+        const rows = [];
 
-        //We'll return [ [5,x],[4,3],[2,1] ];
+        //Create rows until we run out of images
+        let imageIndex = 0, patternIndex = 0;
+        while(imageIndex > -1) {
+          const rowAndIndices = createRow(imageIndex, patternIndex);
+          imageIndex = rowAndIndices.imageIndex;
+          patternIndex = rowAndIndices.patternIndex;
+
+          if(rowAndIndices.row)
+            rows.push(rowAndIndices.row);
+        }
+
+        return rows.length > 0 ? rows.reverse() : undefined;
       },
+      /** 
+       * @return Rows in order of how they should be rendered, excluding the topmost row.
+       */
+      bottomRows() {
+        return rows ? rows.slice(1, rows.length) : [];
+      },
+      /** 
+       * @return The first row that should be rendered.
+       */
       topRow() {
-
+        return rows ? rows[0] : [];
       },
-      remainingRows() {
-
+      /** 
+       * @return The CSS classes that should be applied to all images.
+       */
+      imgClass() {
+        return {};
       },
-      canRender() {
+      /** 
+       * @return Any CSS styling that should be applied to all images. Should be useful
+       *   for any dynamic styles.
+       */
+      imgStyle() {
+        return {};
+      }
+    },
+    methods: {
+      createRow(imageIndex, patternIndex) {
+        const pattern = this.pattern[patternIndex];
+
+        //Integer case
+
+        //Array case
+
+        //Undefined case
 
       }
     }
