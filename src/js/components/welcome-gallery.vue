4a402c5776ea7a4906106f378c610280da72db38
Robert Nill <rmn6979@rit.edu>
2018-03-03 22:38:10 -0500

    Welcome page with flexbox, general updates
    Added some temporary messages to the rest of the site, updated the welcome section with new text and added some responsive CSS. Gonna look at using grids for the larger version.

--- src/js/components/welcome-gallery.vue
+++ src/js/components/welcome-gallery.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
-    <div>
-      <diamond-grid :images="images" :pattern="pattern" width=200 height=200></diamond-grid>
+    <diamond-grid :images="images" :pattern="altPattern" width=120 height=120 
+                  flip-horizontal=true class="hidden-md-and-up altTop"></diamond-grid>
+    <div style="display: flex; justify-content: center">
+      <diamond-grid :images="images" :pattern="pattern" width=120 height=120 
+                    flip-horizontal=true class="hidden-sm-and-down"></diamond-grid>
+      <img src="../../assets/test.png" class="center">
+      <diamond-grid :images="images" :pattern="pattern" width=120 height=120 
+                    class="hidden-sm-and-down"></diamond-grid>
     </div>
   </div>
 </template>
@@ -15,14 +21,30 @@
     components: {DiamondGrid},
     data() {
       return {
-        pattern: [1, 2, ["i", "X", "i"], 2],
-        images: [require("../../assets/test.png"), require("../../assets/test.png"), require("../../assets/test.png"),
-          require("../../assets/test.png"), require("../../assets/test.png"), require("../../assets/test.png"), require("../../assets/test.png")],
+        pattern: [["i", "x"], 1, 2],
+        images: [require("../../assets/test.png"), require("../../assets/test.png"), 
+                require("../../assets/test.png"), require("../../assets/test.png")],
+        altPattern: [["i", "x", "i"], 2],
+        altImages: [require("../../assets/test.png"), require("../../assets/test.png")]
       }
     }
   }
 </script>
 
 <style>
+  .center {
+    -webkit-clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
+    clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
+    display: inline-block;
+    min-width: 250px;
+    min-height: 250px;
+    width: 250px;
+    height: 250px;
+    flex-grow: 0;
+    margin: 0px 60px 0px 60px;
+  }
 
+  .altTop {
+    margin-bottom: -120px;
+  }
 </style>
--- src/js/components/welcome.vue
+++ src/js/components/welcome.vue
@@ -2,22 +2,22 @@
   <div>
     <!-- Website title -->
     <v-divider></v-divider>
+    <br>
     <h1>{{title}}</h1>
 
     <!-- Image gallery -->
     <welcome-gallery></welcome-gallery>
 
     <!-- Description -->
-    <div class="description">{{description}}</div>
+    <div class="description title">{{description}}</div>
 
     <!-- Recent events -->
-    <v-container grid-list-md>
-      <div class="recentEvents" v-for="event in recentEvents">
-        <v-flex class="eventLeft">Left</v-flex>
-        <v-flex class="eventRight">Right</v-flex>
-      </div>
-    </v-container>
+    <div class="recentEvents" v-for="event in recentEvents" :key="event.id">
+      <div class="eventLeft">{{event.description}}</div>
+      <div class="eventRight">{{event.dateText}}</div>
+    </div>
 
+    <br>
     <v-divider></v-divider>
   </div>
 </template>
@@ -41,18 +41,41 @@
 
 <style>
   .description {
+    margin-top: 30px;
+    margin-bottom: 30px;
     margin-left: 20%;
     margin-right: 20%;
   }
   .recentEvents {
     margin-left: 20%;
     margin-right: 20%;
-
+    display: flex;
+    justify-content: space-between;
   }
   .eventLeft {
     text-align: left;
+    font-weight: bold;
   }
   .eventRight {
     text-align: right;
+  }
+  /** CSS for smaller stuff */
+  @media screen and (max-width:960px) {
+    .description {
+      margin-top: 30px;
+      margin-bottom: 0px;
+      margin-left: 5%;
+      margin-right: 5%;
+    }
+    .recentEvents {
+      display: inline;
+      margin-left: 20%;
+      margin-right: 20%;
+    }
+    .eventLeft, .eventRight {
+      text-align: center;
+      margin-left: 5%;
+      margin-right: 5%;
+    }
   }
 </style>
